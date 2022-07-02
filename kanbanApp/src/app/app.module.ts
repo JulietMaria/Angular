f295fb5e46a514446ed1a9e336415a6befe8d3df
JulietMaria <JulietMaria@github.com>
2022-07-02 10:12:30 +0530

    Files updated

---
+++ kanbanApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
